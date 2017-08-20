b39533d01751a42137c4c14bc7f56142e26fe51d
Aaron Robson <ARobsonSE@hotmail.co.uk>
2017-08-20 02:06:11 +0100

    Factored Rule's function out.

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import GameOfLife
 import colourutils
 import counter
 import cellfile
+import rule
 
 '''TODO:
 Test: GameOfLifeGUI
@@ -14,7 +15,7 @@ Test: GameOfLifeGUI
 
 class TestRule(unittest.TestCase):
   def setUp(self):
-    self.support = GameOfLife
+    self.support = rule
     self.widget = self.support.Rule
 
   def test_String(self):
@@ -61,30 +62,30 @@ class TestRule(unittest.TestCase):
     self.assertFalse(rule.IsAliveNextGeneration(False, 1), '')
 
   def test_StringToDigitTuple(self):
-    self.assertEqual(self.widget()._StringToDigitTuple(''), (), '')
-    self.assertEqual(self.widget()._StringToDigitTuple('jasdjf £€$ ;\!"£$%^//&*()\'#;[]\':{:\')'), (), '')
-    self.assertEqual(self.widget()._StringToDigitTuple('93648261'), (1,2,3,4,6,8,9), '')
-    self.assertEqual(self.widget()._StringToDigitTuple('djksd2kdfadfk3kfa;@~}1'), (1,2,3), '')
-    self.assertEqual(self.widget()._StringToDigitTuple('11111111111111111111adjfa33333333jdfj5555'), (1,3,5), '')
+    self.assertEqual(self.support._StringToDigitTuple(''), (), '')
+    self.assertEqual(self.support._StringToDigitTuple('jasdjf £€$ ;\!"£$%^//&*()\'#;[]\':{:\')'), (), '')
+    self.assertEqual(self.support._StringToDigitTuple('93648261'), (1,2,3,4,6,8,9), '')
+    self.assertEqual(self.support._StringToDigitTuple('djksd2kdfadfk3kfa;@~}1'), (1,2,3), '')
+    self.assertEqual(self.support._StringToDigitTuple('11111111111111111111adjfa33333333jdfj5555'), (1,3,5), '')
 
   def test_RuleToString(self):
-    self.assertEqual(self.widget()._RuleToString(((), ())), '/', 'RuleToString Fail: on empty notation.')
-    self.assertEqual(self.widget()._RuleToString(((1,), (2,))), '1/2', 'RuleToString Fail: on standard notation 1 by 1.')
-    self.assertEqual(self.widget()._RuleToString(((1,2,), (3,))), '12/3', 'RuleToString Fail: on standard notation 2 by 1.')
-    self.assertEqual(self.widget()._RuleToString(((1,2,), (3,4,))), '12/34', 'RuleToString Fail: on standard notation extended 2 by 2.')
-    self.assertEqual(self.widget()._RuleToString(((1,), (2,), (3,))), '1/2', 'RuleToString Fail: on more than two rule sections.')
+    self.assertEqual(self.support._RuleToString(((), ())), '/', 'RuleToString Fail: on empty notation.')
+    self.assertEqual(self.support._RuleToString(((1,), (2,))), '1/2', 'RuleToString Fail: on standard notation 1 by 1.')
+    self.assertEqual(self.support._RuleToString(((1,2,), (3,))), '12/3', 'RuleToString Fail: on standard notation 2 by 1.')
+    self.assertEqual(self.support._RuleToString(((1,2,), (3,4,))), '12/34', 'RuleToString Fail: on standard notation extended 2 by 2.')
+    self.assertEqual(self.support._RuleToString(((1,), (2,), (3,))), '1/2', 'RuleToString Fail: on more than two rule sections.')
 
   def test_StringToRule(self):
-    self.assertEqual(self.widget()._StringToRule(), ((3,), (2,3,)), 'StringToRule Fail: default rule incorrect.')
-    self.assertEqual(self.widget()._StringToRule(''), ((), ()), 'StringToRule Fail: on empty notation.')
-    self.assertEqual(self.widget()._StringToRule('/'), ((), ()), 'StringToRule Fail: on only separator notation.')
-    self.assertEqual(self.widget()._StringToRule('1/'), ((1,), ()), 'StringToRule Fail: on first empty.')
-    self.assertEqual(self.widget()._StringToRule('/2'), ((), (2,)), 'StringToRule Fail: on second empty.')
-    self.assertEqual(self.widget()._StringToRule('1/2'), ((1,), (2,)), 'StringToRule Fail: on standard notation 1 by 1.')
-    self.assertEqual(self.widget()._StringToRule('  5  6  /  7  8  \r\n\t!"£$%^&*()'), ((5,6), (7,8)), 'StringToRule Fail: Fails to ignore whitespace and other characters properly.')
-    self.assertEqual(self.widget()._StringToRule('12/3'), ((1,2,), (3,)), 'StringToRule Fail: on standard notation 2 by 1.')
-    self.assertEqual(self.widget()._StringToRule('12/34'), ((1,2,), (3,4,)), 'StringToRule Fail: on standard notation extended 2 by 2.')
-    self.assertEqual(self.widget()._StringToRule('1/2/3'), ((1,), (2,)), 'StringToRule Fail: on more than two rule sections.')
+    self.assertEqual(self.support._StringToRule(), ((3,), (2,3,)), 'StringToRule Fail: default rule incorrect.')
+    self.assertEqual(self.support._StringToRule(''), ((), ()), 'StringToRule Fail: on empty notation.')
+    self.assertEqual(self.support._StringToRule('/'), ((), ()), 'StringToRule Fail: on only separator notation.')
+    self.assertEqual(self.support._StringToRule('1/'), ((1,), ()), 'StringToRule Fail: on first empty.')
+    self.assertEqual(self.support._StringToRule('/2'), ((), (2,)), 'StringToRule Fail: on second empty.')
+    self.assertEqual(self.support._StringToRule('1/2'), ((1,), (2,)), 'StringToRule Fail: on standard notation 1 by 1.')
+    self.assertEqual(self.support._StringToRule('  5  6  /  7  8  \r\n\t!"£$%^&*()'), ((5,6), (7,8)), 'StringToRule Fail: Fails to ignore whitespace and other characters properly.')
+    self.assertEqual(self.support._StringToRule('12/3'), ((1,2,), (3,)), 'StringToRule Fail: on standard notation 2 by 1.')
+    self.assertEqual(self.support._StringToRule('12/34'), ((1,2,), (3,4,)), 'StringToRule Fail: on standard notation extended 2 by 2.')
+    self.assertEqual(self.support._StringToRule('1/2/3'), ((1,), (2,)), 'StringToRule Fail: on more than two rule sections.')
 
   def test_RuleParserFromString_GetRule_RuleToString(self):
     ruleStr = '45/89'
