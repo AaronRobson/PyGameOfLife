ec2950433f8e9d4143572b9d18dd5867cf4a3f69
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-05 22:45:25 +0000

    Factored out counter tests.

--- tests/test_library.py
+++ tests/test_library.py
@@ -3,28 +3,8 @@
 import unittest
 
 import GameOfLife
-import counter
 import cellfile
 
-
-class TestCounter(unittest.TestCase):
-  def setUp(self):
-    self.support = counter
-    self.widget = self.support.Counter()
-
-  def test(self):
-    self.assertEqual(self.widget(), 0, '')
-    self.widget.Inc()
-    self.assertEqual(self.widget(), 1, '')
-    self.widget.Inc()
-    self.assertEqual(self.widget(), 2, '')
-
-    self.assertEqual(int(self.widget), 2, '')
-    self.assertEqual('%d' % (self.widget), '2', '')
-    self.assertEqual('%s' % (self.widget), '2', '')
-
-    self.widget.Reset()
-    self.assertEqual(self.widget(), 0, '')
 
 class TestGameOfLife(unittest.TestCase):
   def setUp(self):
