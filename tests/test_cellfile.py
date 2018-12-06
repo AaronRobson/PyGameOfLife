1cb0931cf4caab54aa5cb85379e0389e6f1bcbc8
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-06 03:08:09 +0000

    Fixed test of LineToCell which returns a generator from map in python3.

--- tests/test_cellfile.py
+++ tests/test_cellfile.py
@@ -6,7 +6,7 @@ import cellfile
 class TestLineToCell(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(cellfile.LineToCell('1,2'), [1, 2])
+        self.assertEqual(list(cellfile.LineToCell('1,2')), [1, 2])
 
 
 if __name__ == "__main__":
