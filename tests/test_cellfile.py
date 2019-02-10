306aaf8f6dd44cb34cb060b28a7dc8207eb1d5c7
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-02-10 22:27:49 +0000

    test 'GetCellsFromText'

--- tests/test_cellfile.py
+++ tests/test_cellfile.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cellfile import LineToCell
+from cellfile import LineToCell, GetCellsFromText
 
 
 class TestLineToCell(unittest.TestCase):
@@ -16,3 +16,32 @@ class TestLineToCell(unittest.TestCase):
 
     def test_pair_with_comment(self):
         self.assertEqual(list(LineToCell('3,4; example comment')), [3, 4])
+
+
+class TestGetCellsFromText(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(list(GetCellsFromText('')), [])
+
+    def test_realisic(self):
+        text = '''; Glider
+            1,0
+            2,1
+            0,2
+            1,2
+            2,2
+
+            ; R-pentomino
+            ; 1,0
+            ; 2,0
+            ; 0,1
+            ; 1,1
+            ; 1,2'''
+        expected = [
+            (1, 0),
+            (2, 1),
+            (0, 2),
+            (1, 2),
+            (2, 2),
+        ]
+        self.assertEqual(list(GetCellsFromText(text)), expected)
