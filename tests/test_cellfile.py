288c3094caaad8a7eda83b328bf2f3f09a3f0036
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-02-10 21:15:47 +0000

    import cellfile functions more specifically in tests

--- tests/test_cellfile.py
+++ tests/test_cellfile.py
@@ -1,18 +1,18 @@
 import unittest
 
-import cellfile
+from cellfile import LineToCell
 
 
 class TestLineToCell(unittest.TestCase):
 
     def test_empty(self):
-        self.assertEqual(list(cellfile.LineToCell('')), [])
+        self.assertEqual(list(LineToCell('')), [])
 
     def test_comment_only(self):
-        self.assertEqual(list(cellfile.LineToCell('; example comment')), [])
+        self.assertEqual(list(LineToCell('; example comment')), [])
 
     def test_pair(self):
-        self.assertEqual(list(cellfile.LineToCell('1,2')), [1, 2])
+        self.assertEqual(list(LineToCell('1,2')), [1, 2])
 
     def test_pair_with_comment(self):
-        self.assertEqual(list(cellfile.LineToCell('3,4; example comment')), [3, 4])
+        self.assertEqual(list(LineToCell('3,4; example comment')), [3, 4])
