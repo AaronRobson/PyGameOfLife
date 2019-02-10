c77107bc64c1c8bc2a7d5114bdcbf2d9f377146d
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-02-10 21:09:22 +0000

    test cellfile's LineToCell with empty and comment lines

--- tests/test_cellfile.py
+++ tests/test_cellfile.py
@@ -5,5 +5,14 @@ import cellfile
 
 class TestLineToCell(unittest.TestCase):
 
-    def test(self):
+    def test_empty(self):
+        self.assertEqual(list(cellfile.LineToCell('')), [])
+
+    def test_comment_only(self):
+        self.assertEqual(list(cellfile.LineToCell('; example comment')), [])
+
+    def test_pair(self):
         self.assertEqual(list(cellfile.LineToCell('1,2')), [1, 2])
+
+    def test_pair_with_comment(self):
+        self.assertEqual(list(cellfile.LineToCell('3,4; example comment')), [3, 4])
