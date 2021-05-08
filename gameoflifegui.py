5107bbd2e799f4daa11913ab11a0db1d57d61a42
Aaron Robson <arobsonse@hotmail.co.uk>
2021-05-08 23:25:05 +0100

    prevent continuously increasing window size

--- gameoflifegui.py
+++ gameoflifegui.py
@@ -389,7 +389,7 @@ class Gui(tk.Tk):
         '''Change the size stored in the canvas when the window is
         resized so ChangeOriginDrag works properly.
         '''
-        self.cnvs['width'], self.cnvs['height'] = event.width-2, event.height-2
+        self.cnvs['width'], self.cnvs['height'] = event.width-4, event.height-4
 
 
 if __name__ == "__main__":
